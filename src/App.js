3f5724bdbe5974326c9c76e83346aa52dcd4e9fc
Kyle Ryu <kyleryu6698@gmail.com>
2023-01-26 23:13:05 +0900

    Navigation 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/login' element={<Login />}></Route>
