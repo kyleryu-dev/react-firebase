6c15955a368824d1b8d94d09a40cf40cad406177
Kyle Ryu <kyleryu6698@gmail.com>
2023-01-27 15:18:16 +0900

    Firebase 프로젝트 설정, 환경변수 관리

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,43 @@
 import styles from './Signup.module.css';
+import { useState } from 'react';
 
-export default function Signup() {
+export default function Singup() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleData = (event) => {
+    if (event.target.type === "email") {
+      setEmail(event.target.value);
+    } else if (event.target.type === "password") {
+      setPassword(event.target.value);
+    } else if (event.target.type === "text") {
+      setDisplayName(event.target.value);
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  }
+
   return (
-    <div>Signup</div>
+    <form className={styles.signup_form} onSubmit={handleSubmit} >
+      <fieldset>
+        <legend>회원가입</legend>
+
+        <label htmlFor="myEmail">email : </label>
+        <input type="email" id="myEmail" required onChange={handleData} value={email} />
+
+        <label htmlFor="myPassWord">password : </label>
+        <input type="password" id="myPassWord" required onChange={handleData} value={password} />
+
+        <label htmlFor="myNickName">닉네임 : </label>
+        <input type="text" id="myNickName" required onChange={handleData} value={displayName} />
+
+        <button type="submit" className={styles.btn}>가입하기</button>
+      </fieldset>
+    </form>
   )
 }
